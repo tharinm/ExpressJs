72d8999fb641dd8e70784f6ff144309033b9b0f9
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-03-04 23:56:04 +0530

    expressjs routes

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next(); //allow the request to coontinue to the next middleware inline
+app.use("/product-page", (req, res, next) => {
+    console.log("This is Product");
+     res.send("<h1>Hello From Product</h1>");
 });
 
-app.use((req, res, next) => {
+
+app.use('/',(req, res, next) => {
     console.log("In another middleware");
-    res.send('<h1>Hello From Express</h1>')
+    res.send('<h1>Hello From Express</h1>');
 });
+
+
 
 
 app.listen(3000);
