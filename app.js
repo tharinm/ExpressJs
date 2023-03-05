3bcc5589e8a3c68f06143049941dc50836451f88
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-03-05 10:46:57 +0530

    404 Eror page Route

--- app.js
+++ app.js
@@ -11,4 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+//404 Eror page not found route
+app.use((req, res, next) => {
+  res.status(404).send("<h1>PAGE NOT FOUND</h1>")
+})
+
 app.listen(3000);
