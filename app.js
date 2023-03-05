650e2ec760c757a9b527a19003d85f8dfdd175ff
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-03-05 11:35:56 +0530

    Route Filtering

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
+const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes=require('./routes/shop')
 
 const bodyParser = require("body-parser");
 
-const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 //404 Eror page not found route
 app.use((req, res, next) => {
-  res.status(404).send("<h1>PAGE NOT FOUND</h1>")
+  res.status(404).send("<h1 style='color:red'>PAGE NOT FOUND</h1>")
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get("/product-page", (req, res, next) => {
+// /admin/product-page =>get
+router.get('/product-page', (req, res, next) => {
   console.log("This is Product");
   res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Submit</button></form>"
+    "<form action='admin/product-page' method='POST'><input type='text' name='title'><button type='submit'>Submit</button></form>"
   );
 });
 
-router.post("/product", (req, res, next) => {
+// /admin/product-page =>POST
+router.post('/product-page', (req, res, next) => {
   //   console.log("In another middleware");
   console.log(req.body);
   res.redirect("/");
